e761dbfb1e51113350fc92719ecd9f71d855e6df
draja <raja-27@hotmail.com>
2014-12-18 18:07:54 +0100

    Arreglando cosas

---
+++ Maverick-api/src/main/java/edu/upc/eetac/dsa/dsaqt1415g3/Maverick/api/model/UsersCollection.java
@@ -0,0 +1,32 @@
+package edu.upc.eetac.dsa.dsaqt1415g3.Maverick.api.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.core.Link;
+
+public class UsersCollection {
+	private List<Link> links;
+	private List<Users> users;
+	
+	public UsersCollection() {
+		super();
+		users = new ArrayList<>();
+	}
+	public List<Link> getLinks() {
+		return links;
+	}
+	public void setLinks(List<Link> links) {
+		this.links = links;
+	}
+	public List<Users> getUsers() {
+		return users;
+	}
+	public void setUsers(List<Users> users) {
+		this.users = users;
+	}
+	public void addUser(Users user){
+		users.add(user);	
+	}
+
+}
