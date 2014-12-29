8141fe4ca3c7ad04d6d7f9c4ce03ffb9d3254a96
DaviD <david_bcn_210@hotmail.com>
2014-12-29 11:04:52 +0100

    resolviendo conflictos

--- Maverick-api/src/main/java/edu/upc/eetac/dsa/dsaqt1415g3/Maverick/api/MediaType.java
+++ Maverick-api/src/main/java/edu/upc/eetac/dsa/dsaqt1415g3/Maverick/api/MediaType.java
@@ -1,6 +1,7 @@
 package edu.upc.eetac.dsa.dsaqt1415g3.Maverick.api;
 
 public interface MediaType {
+	
 	//intercambiamos recursos en un determinado formato de datos (json),de manera que es un derivado de json para indicar el formato
 	public final static String MAVERICK_API_USER = "application/vnd.maverick.api.user+json"; //usuario
 	public final static String MAVERICK_API_USER_COLLECTION = "application/vnd.maverick.api.user.collection+json"; //coleción usuarios
@@ -8,4 +9,5 @@ public interface MediaType {
 	public final static String MAVERICK_API_SONG_COLLECTION = "application/vnd.maverick.api.song.collection+json"; //coleción stings
 	public final static String MAVERICK_API_COMMENT = "application/vnd.maverick.api.comment+json";
 	public final static String MAVERICK_API_ERROR = "application/vnd.dsa.maverick.error+json";
+
 }
