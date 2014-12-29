ab9161b817cd334ac57441373161ba92cda3f896
DaviD <DaviD@DC7>
2014-12-29 11:00:15 +0100

    Solucinando problemas

---
+++ Maverick-api/src/main/java/edu/upc/eetac/dsa/dsaqt1415g3/Maverick/api/MediaType.java
@@ -0,0 +1,11 @@
+package edu.upc.eetac.dsa.dsaqt1415g3.Maverick.api;
+
+public interface MediaType {
+	//intercambiamos recursos en un determinado formato de datos (json),de manera que es un derivado de json para indicar el formato
+	public final static String MAVERICK_API_USER = "application/vnd.maverick.api.user+json"; //usuario
+	public final static String MAVERICK_API_USER_COLLECTION = "application/vnd.maverick.api.user.collection+json"; //coleción usuarios
+	public final static String MAVERICK_API_SONG = "application/vnd.maverick.api.song+json"; 
+	public final static String MAVERICK_API_SONG_COLLECTION = "application/vnd.maverick.api.song.collection+json"; //coleción stings
+	public final static String MAVERICK_API_COMMENT = "application/vnd.maverick.api.comment+json";
+	public final static String MAVERICK_API_ERROR = "application/vnd.dsa.maverick.error+json";
+}
---
+++ Maverick-api/src/main/java/edu/upc/eetac/dsa/dsaqt1415g3/Maverick/api/model/UsersCollection.java
@@ -0,0 +1,42 @@
+package edu.upc.eetac.dsa.dsaqt1415g3.Maverick.api.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.core.Link;
+
+import org.glassfish.jersey.linking.Binding;
+import org.glassfish.jersey.linking.InjectLink;
+import org.glassfish.jersey.linking.InjectLinks;
+import org.glassfish.jersey.linking.InjectLink.Style;
+
+import edu.upc.eetac.dsa.dsaqt1415g3.Maverick.api.MediaType;
+import edu.upc.eetac.dsa.dsaqt1415g3.Maverick.api.UserResource;
+
+public class UsersCollection {
+	
+	
+	private List<Link> links;
+	private List<Users> users;
+	
+	public UsersCollection() {
+		super();
+		users = new ArrayList<>();
+	}
+	public List<Link> getLinks() {
+		return links;
+	}
+	public void setLinks(List<Link> links) {
+		this.links = links;
+	}
+	public List<Users> getUsers() {
+		return users;
+	}
+	public void setUsers(List<Users> users) {
+		this.users = users;
+	}
+	public void addUser(Users user){
+		users.add(user);	
+	}
+
+}
