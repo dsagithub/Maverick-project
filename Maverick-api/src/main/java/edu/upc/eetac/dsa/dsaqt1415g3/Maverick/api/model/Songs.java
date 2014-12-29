11d77175c66e12a6e692fee7af0ad15c6ea6d45f
Felipe <felipe.boix@gmail.com>
2014-12-29 11:39:37 +0100

    resolución conflictos y metodos

---
+++ Maverick-api/src/main/java/edu/upc/eetac/dsa/dsaqt1415g3/Maverick/api/model/Songs.java
@@ -0,0 +1,120 @@
+package edu.upc.eetac.dsa.dsaqt1415g3.Maverick.api.model;
+
+
+
+
+import java.sql.Date;
+import java.util.List;
+
+import javax.ws.rs.core.Link;
+
+public class Songs {
+	
+
+	private List<Link> links;
+	private String songid;
+	private String username;
+	private String song_name;
+	private String album;
+	private String description;
+	private String style;
+	private String songURL;
+	private String likes;
+	private Date last_modified;
+	public Date getLast_modified() {
+		return last_modified;
+	}
+	public void setLast_modified(Date last_modified) {
+		this.last_modified = last_modified;
+	}
+
+	
+
+	public List<Link> getLinks() {
+		return links;
+	}
+	public void setLinks(List<Link> links) {
+		this.links = links;
+	}
+	public String getSongid() {
+		return songid;
+	}
+	public void setSongid(String songid) {
+		this.songid = songid;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getSong_name() {
+		return song_name;
+	}
+	public void setSong_name(String song_name) {
+		this.song_name = song_name;
+	}
+	public String getAlbum() {
+		return album;
+	}
+	public void setAlbum(String album) {
+		this.album = album;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getStyle() {
+		return style;
+	}
+	public void setStyle(String style) {
+		this.style = style;
+	}
+	/*
+	public long getDate() {
+		return date;
+	}
+	public void setDate(long date) {
+		this.date = date;
+	}
+	*/
+	public String getSongURL() {
+		return songURL;
+	}
+	
+	public void setSongURL(String songURL) {
+		this.songURL = songURL;
+	}
+	public String getLikes() {
+		return likes;
+	}
+	public void setLikes(String likes) {
+		this.likes = likes;
+	}
+	private long lastModified;
+	private String text;
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public long getLastModified() {
+		return lastModified;
+	}
+	public void setLastModified(long lastModified) {
+		this.lastModified = lastModified;
+	}
+	public long getCreationTimestamp() {
+		return creationTimestamp;
+	}
+	public void setCreationTimestamp(long creationTimestamp) {
+		this.creationTimestamp = creationTimestamp;
+	}
+	private long creationTimestamp;
+ 
+
+	
+}
---
+++ sql/maverickdb-schema.sql
@@ -0,0 +1,55 @@
+drop database if exists maverickdb;
+create database maverickdb;
+ 
+use maverickdb;
+ 
+create table users (
+	username	varchar(20) not null primary key,
+	userpass	char(32) not null,
+	email		varchar(255) not null,
+	name	varchar(70) not null,
+	description	varchar(500) not  null
+);
+ 
+create table user_roles (
+	username	varchar(20) not null,
+	rolename 			varchar(20) not null,
+	foreign key(username) references users(username) on delete cascade,
+	primary key (username, rolename)
+);
+
+create table songs (
+	songid varchar(50) not null,
+	username	varchar(20) not null,
+	song_name	varchar(100) not null unique,
+	album_name varchar(100),
+	description	varchar(500),
+	style	varchar(50) not  null,
+	last_modified	timestamp default current_timestamp ON UPDATE CURRENT_TIMESTAMP,
+	likes int not null,
+	
+	foreign key(username) references users(username),
+	primary key (username, song_name),
+	index(songid)
+);
+create table comments (
+	commentid int not null auto_increment unique,
+	songid varchar(50) not null,
+	song_name varchar(100) not null,
+	username varchar(50) not null,
+    text varchar(500),
+    time timestamp,
+    foreign key(username) references users (username) on delete cascade,
+	foreign key(song_name) references songs (song_name) on delete cascade
+
+);
+
+
+
+create table follow(
+	followingname varchar(20),
+	followername varchar(20),
+	foreign key(followingname) references users(username) on delete cascade,
+	foreign key(followername) references users(username) on delete cascade,
+	primary key (followingname, followername)
+);
